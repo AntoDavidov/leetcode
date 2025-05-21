7097e85b28c8e7ba373bee827a0b9d99b41093d6
CrustCrafters <youremail@example.com>
2025-05-22 00:08:43 +0200

    First leetcode exercise TwoSum

--- 21.05_28.05/TwoSum.py
+++ 21.05_28.05/TwoSum.py
@@ -1,4 +1,15 @@
-numbers = 12
-number1 = 12
-sum = numbers + number1
-print(sum)
+def twoSum(nums, target):
+    seen = {}
+    for i in range(len(nums)):
+        diff = target - nums[i]
+        if diff in seen:
+            return [seen[diff], i]
+        else:
+            seen[nums[i]] = i
+
+# Example input
+nums = [3,3]
+target = 6
+
+result = twoSum(nums, target)
+print(result)
